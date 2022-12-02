c96a72535a63f85f2e41f1a046482415c1601f85
Kamran Warsi <kwarsi058@gmail.com>
2022-12-02 12:09:14 -0500

    working on GET request and POST

--- routes/posts.py
+++ routes/posts.py
@@ -6,20 +6,22 @@ post_bp = Blueprint('posts', __name__, template_folder ='templates' )
 
 @post_bp.route('/post',methods=['POST'])
 def addPost():
+    user_post_data = request.json
     try:
-        user_post_data = request.json
         post_collection = DB('posts')
         post_collection.save(user_post_data)
         resp = {'msg':'user post found','payload':user_post_data}
     except Exception as e:
-        resp = {'msg':'unable to find user post','error':e}
+        resp = {'msg':'user post not found','error':e}
         
     return jsonify({'resp':resp})
 
-'''
+
 @post_bp.route('/post',methods=['GET'])
 def getPost():
     email = request.args.get('email')
     message = request.args.get('message')
     photo = request.args.get('photo')
-'''
+    getData = DB('posts')
+    
+
