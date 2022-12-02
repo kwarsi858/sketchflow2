d385550c99207b1808dcdcbde8fbc023ef443b1f
Kamran Warsi <kwarsi058@gmail.com>
2022-12-01 22:02:20 -0500

    post json object not serializable

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint
 from routes.users import user_bp
+from routes.posts import post_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(user_bp)
+app.register_blueprint(post_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- routes/posts.py
+++ routes/posts.py
@@ -1,4 +1,25 @@
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, Blueprint, request
 from scripts.db import DB
 
 post_bp = Blueprint('posts', __name__, template_folder ='templates' )
+
+
+@post_bp.route('/post',methods=['POST'])
+def addPost():
+    try:
+        user_post_data = request.json
+        post_collection = DB('posts')
+        post_collection.save(user_post_data)
+        resp = {'msg':'user post found','payload':user_post_data}
+    except Exception as e:
+        resp = {'msg':'unable to find user post','error':e}
+        
+    return jsonify({'resp':resp})
+
+'''
+@post_bp.route('/post',methods=['GET'])
+def getPost():
+    email = request.args.get('email')
+    message = request.args.get('message')
+    photo = request.args.get('photo')
+'''
